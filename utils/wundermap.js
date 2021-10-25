cee9651fa674db2f7e2f7138f738f5ad353f26d5
Max <max.te.lintelo@gmail.com>
2021-10-25 18:53:54 +0200

    Convert pressure from pascal ?? to Inch Mercury

--- utils/wundermap.js
+++ utils/wundermap.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const { parse } = require('path');
 
 const WUNDERMAP_ID = process.env.WUNDERMAP_ID;
 const WUNDERMAP_PW = process.env.WUNDERMAP_PW;
@@ -11,7 +12,10 @@ sendData = function(temperature, humidity, pressure) {
     var uri = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=" + WUNDERMAP_ID + "&PASSWORD=" + WUNDERMAP_PW + "&dateutc=now&action=updateraw";
     uri += "&tempf=" + (parseInt(temperature) * 1.8 + 32);
     uri += "&humidity=" + humidity;
-    uri += "&baromin=" + pressure;
+    // Ervanuitgaande dat druk in Pascal wordt gegeven door sensor
+    // TODO check
+    // To Inch Mercury
+    uri += "&baromin=" + (parseInt(pressure) / 1000 * 29.6133971008);
 
     https.get(uri, (res) => {
         let data = '';
